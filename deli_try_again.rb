3e57f04eade2aa13e8fb4e96cd3d2ba0c28313ca
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 13:06:06 +0000

    automatically backed up by learn

--- deli_try_again.rb
+++ deli_try_again.rb
@@ -2,5 +2,13 @@ katz_deli = []
 
 def line(array)
   if array.length == 0 
-    puts 
+    puts "The line is currently empty."
+  else 
+    array.each_with_index do |name, index|
+      current_line = "The line is currently: "
+      current_line += "#{index + 1}. #{name} "
+    end
+    puts current_line.snip
 end
+
+line(["marco", "john"])
