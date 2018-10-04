150905b746acd71a9f5087e89a9b21af58d9b319
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 13:06:44 +0000

    automatically backed up by learn

--- deli_try_again.rb
+++ deli_try_again.rb
@@ -4,8 +4,8 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
+    current_line = "The line is currently: "
     array.each_with_index do |name, index|
-      current_line = "The line is currently: "
       current_line += "#{index + 1}. #{name} "
     end
     puts current_line.snip
